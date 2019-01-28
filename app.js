b869bb9ddfeb136c496fd1b1f0b6f49131eaf093
Eric Lannan <eric.r.lannan@gmail.com>
2019-01-28 23:07:35 +0000

    added mongolab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes       = require("./routes/index");
 
 
-mongoose.connect("mongodb://localhost/yelp_camp_v13", {useNewUrlParser: true});
+//mongoose.connect("mongodb://localhost/yelp_camp_v13", {useNewUrlParser: true});
+mongoose.connect("mongodb://eric:mosby1@ds257314.mlab.com:57314/erl_yelp_camp", {useNewUrlParser: true});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
